6eac0bb5cf9ff90cbe6db974a20f432659ed7066
sazzad <sazzad.islam@dsinnovators.com>
2017-01-10 13:14:21 +0600

    collection view done with custom image view

---
+++ AlbumViewTest/CollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  CollectionViewCell.swift
+//  AlbumViewTest
+//
+//  Created by sazzad on 1/10/17.
+//  Copyright © 2017 Dynamic Solution Innovators. All rights reserved.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+    
+    
+}
---
+++ AlbumViewTest/NewViewController.swift
@@ -0,0 +1,57 @@
+//
+//  NewViewController.swift
+//  AlbumViewTest
+//
+//  Created by sazzad on 1/10/17.
+//  Copyright © 2017 Dynamic Solution Innovators. All rights reserved.
+//
+
+import UIKit
+
+class NewViewController: UIViewController , UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    
+    
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    let imageArray = [UIImage(named: "Shutter-Release"),
+                      UIImage(named: "logo-1"),
+                      UIImage(named: "logo-2"),
+                      UIImage(named: "logo-3"),
+                      UIImage(named: "logo-4")]
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        //collectionView.frame.cell
+        // Do any additional setup after loading the view.
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return imageArray.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell_new", for: indexPath) as! CollectionViewCell
+        
+        cell.imageView.image = self.imageArray[indexPath.row]
+        
+        
+        
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let width = self.collectionView.frame.width / 3 - 1
+        return CGSize(width: width, height: width)
+    }
+    
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 1.0
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return 1.0
+    }
+
+
+}
